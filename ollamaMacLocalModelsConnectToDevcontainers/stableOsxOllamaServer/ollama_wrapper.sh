3567a142d313432a30c6bdb5c2111d4ec934be7b
Chris Paiano <chris.paiano@gmail.com>
2026-05-21 21:57:41 -0400

    Added keep-alive setting

--- ollamaMacLocalModelsConnectToDevcontainers/stableOsxOllamaServer/ollama_wrapper.sh
+++ ollamaMacLocalModelsConnectToDevcontainers/stableOsxOllamaServer/ollama_wrapper.sh
@@ -8,5 +8,8 @@ ulimit -n 65536
 # Export environment variable for Ollama to ensure it binds to the LAN IP
 export OLLAMA_HOST=0.0.0.0:11434
 
+# Ollama keep alive indefinitely to avoid models randomly shutting down in the middle of work
+export OLLAMA_KEEP_ALIVE=-1
+
 # Execute the ollama serve command
 /opt/homebrew/bin/ollama serve
